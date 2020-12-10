5935205be31e8c6f0b8a629347f3949a6f29d9a3
Garrett Robinson <grobinson3@apple.com>
2020-12-09 21:43:25 -0800

    Day 6, Part 2, Python

--- day06/custom_customs.py
+++ day06/custom_customs.py
@@ -7,13 +7,18 @@ class Individual(str):
 
 
 class Group(list):
-    @property
-    def answered_yes_to(self):
-        return set().union(*[individual.answered_yes_to for individual in self])
-
     @classmethod
     def from_string(cls, s):
-        return cls([Individual(individual) for individual in s.split('\n')])
+        return cls([Individual(individual) for individual in s.strip().split('\n')])
+
+    @property
+    def questions_anyone_answered_yes_to(self):
+        return set().union(*[individual.answered_yes_to for individual in self])
+
+    @property
+    def questions_everyone_answered_yes_to(self):
+        individuals_questions_with_yes_answers = [individual.answered_yes_to for individual in self]
+        return individuals_questions_with_yes_answers[0].intersection(*individuals_questions_with_yes_answers[1:])
 
 
 class Groups(list):
@@ -27,8 +32,10 @@ def main():
         input = f.read()
 
     groups = Groups.from_string(input)
-    sum_group_counts = sum([len(group.answered_yes_to) for group in groups])
-    print(sum_group_counts)
+    sum_group_anyone_counts = sum([len(group.questions_anyone_answered_yes_to) for group in groups])
+    sum_group_everyone_counts = sum([len(group.questions_everyone_answered_yes_to) for group in groups])
+    print(sum_group_anyone_counts)
+    print(sum_group_everyone_counts)
 
 
 if __name__ == '__main__':
