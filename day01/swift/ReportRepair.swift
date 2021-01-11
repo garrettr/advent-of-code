02ac017f2ac125cc7e7a46dfe6f6175c35a4e401
Garrett Robinson <grobinson3@apple.com>
2021-01-10 17:51:21 -0800

    Read input from input.txt

--- day01/swift/ReportRepair.swift
+++ day01/swift/ReportRepair.swift
@@ -1,4 +1,7 @@
+import Foundation
+
 let example = [1721, 979, 366, 299, 675, 1456]
+let inputFileName = "../input.txt"
 
 func find_two_entries_that_sum_to_2020(in numbers: [Int]) -> (a: Int, b: Int) {
     for (index, value) in numbers.enumerated() {
@@ -18,3 +21,8 @@ func solve_part_one(for numbers: [Int]) -> Int {
 }
 
 assert(solve_part_one(for: example) == 514579)
+
+let input = try! String(contentsOfFile: inputFileName)
+let inputLines = input.split(separator: "\n")
+let inputNumbers: [Int] = inputLines.map { Int($0)! }
+print("solution for part 1: \(solve_part_one(for: inputNumbers))")
