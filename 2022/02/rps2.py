cc4100fd6e1f016a672933ca49e8af8094f0c565
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-11-14 13:51:56 -0800

    2022, Day 2, Part 2, Python

---
+++ 2022/02/rps2.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+from enum import Enum
+
+
+class Hand(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+    @classmethod
+    def from_char(cls, c):
+        if c == "A":
+            return cls.ROCK
+        elif c == "B":
+            return cls.PAPER
+        elif c == "C":
+            return cls.SCISSORS
+        raise ValueError
+
+
+class Outcome(Enum):
+    WIN = 1
+    LOSE = 2
+    DRAW = 3
+
+    @classmethod
+    def from_char(cls, c):
+        if c == "X":
+            return cls.LOSE
+        elif c == "Y":
+            return cls.DRAW
+        elif c == "Z":
+            return cls.WIN
+
+
+def score_round(opponent, outcome):
+    if outcome is Outcome.DRAW:
+        score = 3
+        hand = opponent
+    elif outcome is Outcome.WIN:
+        score = 6
+        if opponent is Hand.ROCK:
+            hand = Hand.PAPER
+        elif opponent is Hand.PAPER:
+            hand = Hand.SCISSORS
+        elif opponent is Hand.SCISSORS:
+            hand = Hand.ROCK
+        else:
+            raise ValueError
+    elif outcome is Outcome.LOSE:
+        score = 0
+        if opponent is Hand.ROCK:
+            hand = Hand.SCISSORS
+        elif opponent is Hand.PAPER:
+            hand = Hand.ROCK
+        elif opponent is Hand.SCISSORS:
+            hand = Hand.PAPER
+        else:
+            raise ValueError
+    else:
+        raise ValueError
+
+    return score + hand.value
+
+
+def main():
+    total_score = 0
+
+    with open("input.txt") as f:
+        for line in f:
+            chars = line.strip().split()
+            opponent = Hand.from_char(chars[0])
+            outcome = Outcome.from_char(chars[1])
+            total_score += score_round(opponent, outcome)
+
+    print(total_score)
+
+
+if __name__ == "__main__":
+    main()
