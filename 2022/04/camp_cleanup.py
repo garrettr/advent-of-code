95001eef5d7e95ba4928e7e31a877bc754901e1c
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-11-15 20:18:44 -0800

    2022, Day 4, Part 2, Python

--- 2022/04/camp_cleanup.py
+++ 2022/04/camp_cleanup.py
@@ -6,6 +6,13 @@ def fully_contained(pair):
     return (a[0] <= b[0] and a[1] >= b[1]) or (b[0] <= a[0] and b[1] >= a[1])
 
 
+def overlapping(pair):
+    a, b = pair
+    larger, smaller = (a, b) if a[1] - a[0] > b[1] - b[0] else (b, a)
+    larger_range = range(larger[0], larger[1] + 1)
+    return smaller[0] in larger_range or smaller[1] in larger_range
+
+
 def parse(file):
     pairs = []
     for line in file:
@@ -18,15 +25,16 @@ def parse(file):
     return pairs
 
 
-def part1(file):
-    pairs = parse(file)
-    fully_contained_pairs_count = 0
-    for pair in pairs:
-        if fully_contained(pair):
-            fully_contained_pairs_count += 1
-    return fully_contained_pairs_count
+def part1(pairs):
+    return sum([fully_contained(pair) for pair in pairs])
+
+
+def part2(pairs):
+    return sum([overlapping(pair) for pair in pairs])
 
 
 if __name__ == "__main__":
     with open("input.txt") as file:
-        print(part1(file))
+        pairs = parse(file)
+    print(part1(pairs))
+    print(part2(pairs))
