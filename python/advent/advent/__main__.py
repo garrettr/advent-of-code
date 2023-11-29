643f49be0ec5eb3d927b8929f4da409234426a21
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-11-29 10:42:32 -0500

    Initial sketch of script to automate setup for a new day's puzzle

--- python/advent/advent/__main__.py
+++ python/advent/advent/__main__.py
@@ -1,8 +1,62 @@
 #!/usr/bin/env python3
+import argparse
+
+from .puzzle import challenges_path, challenge_path
+
+
+def download_input_files(year, day, path):
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
+    # TODO: Oauth
+    pass
+
+
+def language_specific_setup(language, year, day, path):
+    # If Python, copy template file
+    pass
+
+
+def new(language: str, year: int, day: int):
+    """Prepare for a new day's challenge."""
+    path = challenges_path(language)
+    if not path.exists():
+        res = input(f"Challenges directory {path} does not exist, create it? (y/n)")
+        if res.lower() == "y":
+            path.mkdir(parents=True)
+        else:
+            return
+
+    path = challenge_path(language, year, day)
+    if path.exists():
+        res = input(f"Challenge directory {path} already exists, replace it? (y/n)")
+        if res.lower() == "y":
+            path.rmdir()
+        else:
+            return
+    path.mkdir(parents=True)
+    print(f"Created challenge directory {path}...")
+
+    download_input_files(year, day, path)
+    language_specific_setup(language, year, day, path)
 
 
 def main():
-    print("Hello from advent __main__.py")
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(description="Advent of Code helper tool")
+    subparsers = parser.add_subparsers(
+        dest="subcommand",
+        title="subcommands",
+    )
+
+    # Create a subparser for the "new" subcommand
+    subcmd_new = subparsers.add_parser("new", help="Prepare for a new day's challenge")
+    subcmd_new.add_argument("language", choices=["python"], help="Language to use")
+    subcmd_new.add_argument("year", type=int, help="Year of the day to create")
+    subcmd_new.add_argument("day", type=int, help="Day to create")
+
+    args = parser.parse_args()
+
+    if args.subcommand == "new":
+        new(args.language, args.year, args.day)
 
 
 if __name__ == "__main__":
---
+++ python/advent/advent/puzzle.py
@@ -0,0 +1,27 @@
+import pathlib
+import subprocess
+
+
+def repo_root():
+    """Return the path to the root of the Git repository containing this file."""
+    try:
+        result = subprocess.run(
+            ["git", "rev-parse", "--show-toplevel"],
+            capture_output=True,
+            check=True,
+            cwd=pathlib.Path(__file__).parent,
+        )
+    except subprocess.CalledProcessError as e:
+        print(e.stderr.decode("utf-8"))
+        raise
+    return pathlib.Path(result.stdout.decode("utf-8").strip())
+
+
+def challenges_path(language: str) -> pathlib.Path:
+    """Return the path to the subdirectory of solutions to challenges written in `language`."""
+    return repo_root() / language
+
+
+def challenge_path(language: str, year: int, day: int):
+    """Return the path to the subdirectory containing the solution for a specific challenge."""
+    return challenges_path(language) / str(year) / f"{day:02d}"
