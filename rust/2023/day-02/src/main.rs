e5a177e93b96853a9ce968d514d9783697e97415
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-04 13:18:23 -0800

    Improvements to 2023 Day 2 Rust solution
    - Use `once_cell` to avoid repeatedly compiling regexs, per best
    practice described in the [regex crate docs][0].
    
    - Use a `HashMap` to find the max cubes per color. This is probably
    less efficient than the previous approach of just using a dedicated
    `u32` counter for each color, but it's more general, reduces code
    duplication, and was good practice for using `HashMaps`, the `Entry`
    API, and dealing with associated borrowing/lifetime issues.
    
    [0]: https://docs.rs/regex/latest/regex/#avoid-re-compiling-regexes-especially-in-a-loop

--- rust/2023/day-02/src/main.rs
+++ rust/2023/day-02/src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use {once_cell::sync::Lazy, regex::Regex, std::collections::HashMap};
 
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
@@ -16,32 +16,32 @@ struct Game {
     blue: u32,
 }
 
+static GAME_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^Game (?<id>\d+)").unwrap());
+static CUBES_RE: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"(?<num>\d+) (?<color>red|green|blue)").unwrap());
+
 fn parse_games(input: &str) -> Vec<Game> {
     let mut games = Vec::new();
-    let game_re = Regex::new(r"^Game (?<id>\d+)").unwrap();
-    let cubes_re = Regex::new(r"(?<num>\d+) (?<color>red|green|blue)").unwrap();
 
     for line in input.lines() {
-        let capture = game_re.captures(line).unwrap();
+        let capture = GAME_RE.captures(line).unwrap();
         let id = capture["id"].parse::<u32>().unwrap();
 
-        let mut red = 0;
-        let mut green = 0;
-        let mut blue = 0;
-        for capture in cubes_re.captures_iter(line) {
-            match &capture["color"] {
-                "red" => red = red.max(capture["num"].parse::<u32>().unwrap()),
-                "green" => green = green.max(capture["num"].parse::<u32>().unwrap()),
-                "blue" => blue = blue.max(capture["num"].parse::<u32>().unwrap()),
-                _ => panic!("Unexpected color"),
-            }
+        let mut colors: HashMap<String, u32> = HashMap::with_capacity(3);
+        for capture in CUBES_RE.captures_iter(line) {
+            let num = capture["num"].parse::<u32>().unwrap();
+            let color = capture.name("color").unwrap().as_str().to_owned();
+            colors
+                .entry(color)
+                .and_modify(|count| *count = (*count).max(num))
+                .or_insert(num);
         }
 
         games.push(Game {
-            id,
-            red,
-            green,
-            blue,
+            id: id,
+            red: colors["red"],
+            green: colors["green"],
+            blue: colors["blue"],
         });
     }
 
