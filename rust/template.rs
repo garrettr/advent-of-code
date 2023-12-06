29f0b406da805305665a551a406cc619b0359d6e
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-05 22:51:54 -0800

    Add basic template of boilerplate for Rust solutions

---
+++ rust/template.rs
@@ -0,0 +1,28 @@
+const EXAMPLE: &str = include_str!("example.txt");
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    dbg!(part1(EXAMPLE));
+    // dbg!(part2(INPUT));
+}
+
+fn part1(input: &str) -> () {}
+
+fn part2(input: &str) -> () {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(EXAMPLE), ());
+        // assert_eq!(part1(INPUT), ());
+    }
+
+    #[test]
+    fn test_part2() {
+        // assert_eq!(part2(EXAMPLE), ()));
+        // assert_eq!(part2(INPUT), ()));
+    }
+}
