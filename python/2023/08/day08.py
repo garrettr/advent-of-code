cebdac001006c6867336c530b925527a25644f86
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-09 12:17:18 -0800

    2023, Day 8, Part 2, Python

--- python/2023/08/day08.py
+++ python/2023/08/day08.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from pprint import pprint
 import itertools
+import math
 import re
 import unittest
 
@@ -16,19 +17,26 @@ class Map:
     instructions: str
     nodes: dict[str, dict[str, str]]
 
-    def navigate(self) -> int:
-        current = "AAA"
+    def navigate(self, start="AAA", end="ZZZ") -> int:
+        current = start
         steps = 0
         for instruction in itertools.cycle(self.instructions):
             current = self.nodes[current][instruction]
             steps += 1
-            if current == "ZZZ":
+            if current.endswith(end):
                 return steps
+
+    def navigate2(self):
+        start_nodes, end_nodes = [
+            [key for key in self.nodes.keys() if key.endswith(c)] for c in ("A", "Z")
+        ]
+        path_lens = [self.navigate(start, "Z") for start in start_nodes]
+        return math.lcm(*path_lens)
 
 
 def parse(input: str) -> Map:
     instructions, node_definitions = input.split("\n\n")
-    node_re = re.compile(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)")
+    node_re = re.compile(r"([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)")
     nodes = {}
     for node_definition in node_definitions.splitlines():
         node, left, right = node_re.match(node_definition).groups()
@@ -42,13 +50,15 @@ def part1(input: str) -> int:
 
 
 def part2(input: str):
-    pass
+    map = parse(input)
+    return map.navigate2()
 
 
 class TestDay(unittest.TestCase):
     def setUp(self):
         self.example = get_puzzle_input(YEAR, DAY, "example.txt")
         self.example2 = get_puzzle_input(YEAR, DAY, "example2.txt")
+        self.example3 = get_puzzle_input(YEAR, DAY, "example3.txt")
         self.input = get_puzzle_input(YEAR, DAY)
 
     def test_part1(self):
@@ -56,9 +66,9 @@ class TestDay(unittest.TestCase):
         self.assertEqual(part1(self.example2), 6)
         self.assertEqual(part1(self.input), 19667)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example3), 6)
+        self.assertEqual(part2(self.input), 19185263738117)
 
 
 if __name__ == "__main__":
