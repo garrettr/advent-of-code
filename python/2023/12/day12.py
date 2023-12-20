02a9b51af40052b5035b00291577f1ae47ee66ee
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-19 20:27:16 -0800

    2023, Day 12, Part 1, Python
    Credit to [savbell][0] for their solution!
    
    [0]: https://github.com/savbell/advent-of-code-one-liners/blob/master/2023/day-12.py

--- python/2023/12/day12.py
+++ python/2023/12/day12.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from dataclasses import dataclass
+from itertools import product
+import re
 import unittest
 
 from advent import get_puzzle_input
@@ -8,64 +9,36 @@ YEAR = 2023
 DAY = 12
 
 
-@dataclass
-class Row:
-    condition_records: str
-    damaged_groups: list[int]
-
-
-def parse(input: str) -> list[Row]:
+def parse(input):
     return [
-        Row(a, [int(n) for n in b.split(",")])
+        (a, [int(n) for n in b.split(",")])
         for a, b in (line.split(" ") for line in input.splitlines())
     ]
 
 
-def backtrack(a: list, k: int, input):
-    print(a, k)
-    if is_a_solution(a, k, input):
-        process_solution(a, k, input)
-    else:
-        k += 1
-        for candidate in construct_candidates(a, k, input):
-            a[k] = candidate
-            backtrack(a, k, input)
+def count_arrangements(row) -> int:
+    springs, counts = row
+    results = set()
 
+    possibilities = []
+    for spring in springs:
+        if spring == "?":
+            possibilities.append(["#", "."])
+        else:
+            possibilities.append([spring])
 
-def get_damaged_groups(a: list[str]):
-    return [len(s) for s in "".join(a).split(".") if s]
+    for combo in product(*possibilities):
+        candidate = "".join(combo)
+        matches = re.findall(r"#+", candidate)
+        match_lengths = [len(x) for x in matches]
+        if match_lengths == counts:
+            results.add(candidate)
 
-
-def valid_arrangement(a: list[str], damaged_groups: list[int]):
-    return get_damaged_groups(a) == damaged_groups
-
-
-def is_a_solution(a: list, k: int, input: Row):
-    return k == len(input.condition_records) - 1
-
-
-def construct_candidates(a: list, k: int, input: Row):
-    if input.condition_records[k] == "?":
-        return [".", "#"]
-    return [input.condition_records[k]]
-
-
-part1_num_solutions = 0
-
-
-def process_solution(a: list, k: int, input: Row):
-    if valid_arrangement(a, input.damaged_groups):
-        global part1_num_solutions
-        part1_num_solutions += 1
+    return len(results)
 
 
 def part1(input: str):
-    rows = parse(input)
-    for i, row in enumerate(rows):
-        print(i)
-        a = list(row.condition_records)
-        backtrack(a, -1, row)
-    return part1_num_solutions
+    return sum(count_arrangements(row) for row in parse(input))
 
 
 def part2(input: str):
@@ -79,7 +52,7 @@ class Test(unittest.TestCase):
 
     def test_part1(self):
         self.assertEqual(part1(self.example), 21)
-        # self.assertEqual(part1(self.input), None)
+        self.assertEqual(part1(self.input), 7350)
 
     # def test_part2(self):
     #     self.assertEqual(part2(self.example), None)
