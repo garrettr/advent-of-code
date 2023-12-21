5fc559d30e49c061538def1dd044e1dfa6315bc9
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-21 12:59:36 -0800

    2023, Day 13, Part 2, Python
    Switched into study mode for this one and continued to adapt
    https://advent-of-code.xavd.id/writeups/2023/day/13/.

--- python/2023/13/day13.py
+++ python/2023/13/day13.py
@@ -7,32 +7,44 @@ YEAR = 2023
 DAY = 13
 
 
-def reflection_row(block: list[str]) -> int:
+def distance(l: str, r: str) -> int:
+    return sum(a != b for a, b in zip(l, r))
+
+
+def reflection_row(block: list[str], distance_to_match: int) -> int:
     for idx in range(len(block)):
         if idx == 0:
             continue
 
-        if all(l == r for l, r in zip(reversed(block[:idx]), block[idx:])):
+        if (
+            sum(distance(l, r) for l, r in zip(reversed(block[:idx]), block[idx:]))
+            == distance_to_match
+        ):
             return idx
 
     return 0
 
 
+def score_block(block: str, distance_to_match: int) -> int:
+    rows = block.splitlines()
+    if row := reflection_row(rows, distance_to_match):
+        return 100 * row
+
+    if col := reflection_row(list(zip(*rows)), distance_to_match):
+        return col
+
+    raise ValueError("no reflection found!")
+
+
 def part1(input: str):
-    vertical_reflections = horizontal_reflections = 0
-    for pattern in (section.splitlines() for section in input.split("\n\n")):
-        horizontal = reflection_row(pattern)
-        vertical = reflection_row(list(zip(*pattern)))
-        vertical_reflections += vertical if vertical else 0
-        horizontal_reflections += 100 * horizontal if horizontal else 0
-    return vertical_reflections + horizontal_reflections
+    return sum(score_block(block, 0) for block in input.split("\n\n"))
 
 
 def part2(input: str):
-    pass
+    return sum(score_block(block, 1) for block in input.split("\n\n"))
 
 
-class TestDay(unittest.TestCase):
+class TestDay13(unittest.TestCase):
     def setUp(self):
         self.example = get_puzzle_input(YEAR, DAY, "example.txt")
         self.input = get_puzzle_input(YEAR, DAY)
@@ -41,9 +53,9 @@ class TestDay(unittest.TestCase):
         self.assertEqual(part1(self.example), 405)
         self.assertEqual(part1(self.input), 29213)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 400)
+        self.assertEqual(part2(self.input), 37453)
 
 
 if __name__ == "__main__":
