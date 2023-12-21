6da339e49ae1e70731d48dbd07cad85c207cbd88
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-21 12:40:56 -0800

    Simplify solution for 2023, Day 13, Part 1, Python
    Credit to https://advent-of-code.xavd.id/writeups/2023/day/13/ for an
    extremely elegant and Pythonic approach to testing for reflections.

--- python/2023/13/day13.py
+++ python/2023/13/day13.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-from itertools import pairwise
-from pprint import pprint
 import unittest
 
 from advent import get_puzzle_input
@@ -9,55 +7,25 @@ YEAR = 2023
 DAY = 13
 
 
-def find_potential_reflections(pattern):
-    return [i for i, pair in enumerate(pairwise(pattern)) if pair[0] == pair[1]]
+def reflection_row(block: list[str]) -> int:
+    for idx in range(len(block)):
+        if idx == 0:
+            continue
 
+        if all(l == r for l, r in zip(reversed(block[:idx]), block[idx:])):
+            return idx
 
-def valid_reflection(pattern, reflection):
-    steps = 1
-    for i in range(reflection):
-        lo = reflection - i - 1
-        hi = reflection + i + 2
-        try:
-            if pattern[lo] != pattern[hi]:
-                return False
-        except IndexError:
-            break
-        steps += 1
-    return steps
-
-
-def find_reflection(pattern):
-    candidate = None
-    for reflection in find_potential_reflections(pattern):
-        if width := valid_reflection(pattern, reflection):
-            if not candidate or candidate[1] < width:
-                candidate = (reflection, width)
-    return candidate
+    return 0
 
 
 def part1(input: str):
-    patterns = [section.splitlines() for section in input.split("\n\n")]
-    reflections = []
-    for i, pattern in enumerate(patterns):
-        vref = find_reflection(list(zip(*pattern)))
-        href = find_reflection(pattern)
-
-        if vref and href:
-            if vref[1] > href[1]:
-                reflections.append(("v", vref[0]))
-            else:
-                reflections.append(("h", href[0]))
-        elif vref:
-            reflections.append(("v", vref[0]))
-        elif href:
-            reflections.append(("h", href[0]))
-        else:
-            # raise ValueError(f"No reflection found for pattern: \n{'\n'.join(pattern)}")
-            print(f"No reflection found for {i}: \n{'\n'.join(pattern)}\n")
-
-    # pprint(reflections)
-    return sum(i + 1 if type == "v" else (i + 1) * 100 for type, i in reflections)
+    vertical_reflections = horizontal_reflections = 0
+    for pattern in (section.splitlines() for section in input.split("\n\n")):
+        horizontal = reflection_row(pattern)
+        vertical = reflection_row(list(zip(*pattern)))
+        vertical_reflections += vertical if vertical else 0
+        horizontal_reflections += 100 * horizontal if horizontal else 0
+    return vertical_reflections + horizontal_reflections
 
 
 def part2(input: str):
