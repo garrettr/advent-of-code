17c5f7293d2a54d9c5bf18072513d03afa363250
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-30 19:26:27 -0800

    2023, Day 18, Part 1, Python

---
+++ python/2023/18/day18.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+from itertools import pairwise
+from typing import Literal
+import unittest
+
+from advent import get_puzzle_input
+
+YEAR = 2023
+DAY = 18
+
+LatticePoint = tuple[int, int]
+Direction = Literal["U", "D", "L", "R"]
+
+DIRECTION_OFFSETS: dict[Direction, LatticePoint] = {
+    "U": (-1, 0),
+    "R": (0, 1),
+    "D": (1, 0),
+    "L": (0, -1),
+}
+
+
+def add_points(a: LatticePoint, b: LatticePoint) -> LatticePoint:
+    return a[0] + b[0], a[1] + b[1]
+
+
+def sub_points(a: LatticePoint, b: LatticePoint) -> LatticePoint:
+    return a[0] - b[0], a[1] - b[1]
+
+
+def multiply_point_by_scalar(point: LatticePoint, scalar: int) -> LatticePoint:
+    return point[0] * scalar, point[1] * scalar
+
+
+@dataclass
+class Step:
+    direction: Direction
+    distance: int
+    color: str
+
+    def starting_from(self, start: LatticePoint) -> LatticePoint:
+        return add_points(
+            start,
+            multiply_point_by_scalar(DIRECTION_OFFSETS[self.direction], self.distance),
+        )
+
+
+Plan = list[Step]
+
+
+def parse(input: str) -> Plan:
+    return [
+        Step(direction, int(distance), color.strip("()#"))
+        for direction, distance, color in (
+            line.split(" ") for line in input.splitlines()
+        )
+    ]
+
+
+def boundary_points(plan: Plan) -> list[LatticePoint]:
+    points = [(0, 0)]
+    for step in plan:
+        points.append(step.starting_from(points[-1]))
+    return points
+
+
+def path_length(a: LatticePoint, b: LatticePoint) -> int:
+    diff = sub_points(a, b)
+    length = diff[0] if diff[0] != 0 else diff[1]
+    return abs(length)
+
+
+def area(outline: list[LatticePoint]) -> int:
+    """
+    Uses the shoelace formula to find the area of the outlined polygon.
+    """
+    assert len(outline) > 2 and outline[0] == outline[-1]
+    return (
+        abs(
+            sum(
+                (a[0] * b[1]) - (b[0] * a[1] + path_length(a, b))
+                for a, b in pairwise(outline)
+            )
+            // 2
+        )
+        + 1
+    )
+
+
+def part1(input: str):
+    dig_plan = parse(input)
+    outline = boundary_points(dig_plan)
+    return area(outline)
+
+
+def part2(input: str):
+    pass
+
+
+class TestDay18(unittest.TestCase):
+    def setUp(self):
+        self.example = get_puzzle_input(YEAR, DAY, "example.txt")
+        self.input = get_puzzle_input(YEAR, DAY)
+
+    def test_part1(self):
+        self.assertEqual(part1(self.example), 62)
+        self.assertEqual(part1(self.input), 48400)
+
+    # def test_part2(self):
+    #     self.assertEqual(part2(self.example), None)
+    #     self.assertEqual(part2(self.input), None)
+
+
+if __name__ == "__main__":
+    # unittest.main()
+    print(part1(get_puzzle_input(YEAR, DAY)))
