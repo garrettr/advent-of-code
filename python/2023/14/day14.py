1a046ac5643accaab9d2802ebfcc56927bb9dde8
Garrett Robinson <garrett.f.robinson@gmail.com>
2023-12-31 21:14:55 -0800

    WIP: 2023, Day 14, Part 2, Python
    I think this state cycle finding general approach is sound, but for
    some reason none of the states match the expected total load on the
    north supports for the example (64).

--- python/2023/14/day14.py
+++ python/2023/14/day14.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from enum import Enum
 from pprint import pprint
 import unittest
 
@@ -8,9 +9,8 @@ YEAR = 2023
 DAY = 14
 
 
-def part1(input: str):
-    rows = input.splitlines()
-    cols = zip(*rows)
+def total_load_on_north_support_beams(platform: list[str]):
+    cols = zip(*platform)
 
     # For each column, split into sections delimited by cube-shaped rocks (#) or the edges of the platform.
     # Keep track of the starting index of each section.
@@ -36,8 +36,108 @@ def part1(input: str):
     return total_load
 
 
+def part1(input: str):
+    return total_load_on_north_support_beams(input.splitlines())
+
+
+class GridItem(Enum):
+    EMPTY_SPACE = 0
+    ROUNDED_ROCK = 1
+    CUBE_SHAPED_ROCK = 2
+
+    @classmethod
+    def from_str(cls, s: str):
+        return cls(".O#".index(s))
+
+
+Row = tuple[GridItem, ...]
+Grid = list[Row]
+
+
+def parse_grid(raw_grid: list[str]) -> Grid:
+    return [Row([GridItem.from_str(c) for c in line]) for line in raw_grid]
+
+
+def tilt(row: Row) -> Row:
+    "Tilt row to the left"
+    tilted = []
+    num_rounded_rocks = num_empty_spaces = 0
+
+    for item in row:
+        match item:
+            case GridItem.EMPTY_SPACE:
+                num_empty_spaces += 1
+            case GridItem.ROUNDED_ROCK:
+                num_rounded_rocks += 1
+            case GridItem.CUBE_SHAPED_ROCK:
+                tilted.extend([GridItem.ROUNDED_ROCK for _ in range(num_rounded_rocks)])
+                tilted.extend([GridItem.EMPTY_SPACE for _ in range(num_empty_spaces)])
+                num_rounded_rocks = num_empty_spaces = 0
+                tilted.append(GridItem.CUBE_SHAPED_ROCK)
+
+    if num_empty_spaces or num_rounded_rocks:
+        tilted.extend([GridItem.ROUNDED_ROCK for _ in range(num_rounded_rocks)])
+        tilted.extend([GridItem.EMPTY_SPACE for _ in range(num_empty_spaces)])
+
+    return Row(tilted)
+
+
+def cycle(platform: Grid) -> Grid:
+    "Return the result of tilting the platform North, then West, then South, then East."
+    # North
+    transpose = zip(*platform)
+    platform = [tilt(row) for row in transpose]
+    # West
+    transpose = zip(*platform)
+    platform = [tilt(row) for row in transpose]
+    # South
+    transpose = [tuple(reversed(row)) for row in zip(*platform)]
+    platform = [tilt(row) for row in transpose]
+    # East
+    platform = [tuple(reversed(row)) for row in platform]
+    transpose = [tuple(reversed(row)) for row in zip(*platform)]
+    platform = [tilt(row) for row in transpose]
+    # Return to original orientation
+    return [tuple(reversed(row)) for row in platform]
+
+
+def print_grid(grid: Grid) -> None:
+    for line in grid:
+        print("".join(".O#"[item.value] for item in line))
+
+
 def part2(input: str):
-    pass
+    grid = parse_grid(input.splitlines())
+    states = {}
+    state_cycle_start = state_cycle_end = None
+    cycles = 1_000_000_000
+    for i in range(cycles):
+        if i % 1000 == 0:
+            print(f"{i}/{cycles}")
+
+        tuple_grid = tuple(grid)
+        if tuple_grid in states:
+            state_cycle_start = states[tuple_grid]
+            state_cycle_end = i
+            states[tuple_grid] = i
+            break
+
+        states[tuple_grid] = i
+        grid = cycle(grid)
+
+    state_cycle_step_after_cycles = cycles % (state_cycle_end - state_cycle_start)
+    states_by_step = {step: state for state, step in states.items()}
+    grid = states_by_step[state_cycle_step_after_cycles]
+    for step, state in states_by_step.items():
+        print(
+            step,
+            total_load_on_north_support_beams(
+                ["".join([".O#"[item.value] for item in line]) for line in state]
+            ),
+        )
+    return total_load_on_north_support_beams(
+        ["".join([".O#"[item.value] for item in line]) for line in grid]
+    )
 
 
 class TestDay14(unittest.TestCase):
@@ -49,9 +149,9 @@ class TestDay14(unittest.TestCase):
         self.assertEqual(part1(self.example), 136)
         self.assertEqual(part1(self.input), 105249)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 64)
+        # self.assertEqual(part2(self.input), None)
 
 
 if __name__ == "__main__":
