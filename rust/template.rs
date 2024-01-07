ddebaaa6061b82e976f6f2ea9f564a0469a9ff41
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-01-06 18:54:20 -0800

    Rearrange Rust template so main() and tests are together at the end

--- rust/template.rs
+++ rust/template.rs
@@ -1,14 +1,14 @@
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
+fn part1(input: &str) -> () {}
+
+fn part2(input: &str) -> () {}
+
 fn main() {
     dbg!(part1(EXAMPLE));
     // dbg!(part2(INPUT));
 }
-
-fn part1(input: &str) -> () {}
-
-fn part2(input: &str) -> () {}
 
 #[cfg(test)]
 mod tests {
