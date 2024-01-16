b47a162d7600e81a7011dc06bed1fc26a935733a
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-01-16 10:27:17 -0800

    2023, Day 13, Part 2, Rust
    Also improves error handling by changing `summarize()` to return a
    Result with a descriptive error instead of simply `panic!`ing.

--- rust/2023/day-13/src/main.rs
+++ rust/2023/day-13/src/main.rs
@@ -9,13 +9,13 @@ fn distance(l: &str, r: &str) -> i32 {
         .sum()
 }
 
-fn reflection_row(pattern: &[&str]) -> Option<i32> {
+fn reflection_row(pattern: &[&str], expected_distance: i32) -> Option<i32> {
     for i in 1..pattern.len() {
         let (left, right) = pattern.split_at(i);
         if zip(left.iter().rev(), right)
             .map(|(&l, &r)| distance(l, r))
             .sum::<i32>()
-            == 0
+            == expected_distance
         {
             return Some(i as i32);
         }
@@ -39,32 +39,54 @@ fn transpose(input: &[&str]) -> Vec<String> {
     transposed
 }
 
-fn summarize(pattern: &str) -> i32 {
+#[derive(Debug)]
+enum SummaryError {
+    ReflectionNotFound(String),
+}
+
+// To satisfy the `Error` trait a `Display` implementation is also needed.
+impl std::fmt::Display for SummaryError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ReflectionNotFound(s) => write!(f, "Reflection not found for pattern: {}", s),
+        }
+    }
+}
+
+fn summarize(pattern: &str, expected_distance: i32) -> Result<i32, SummaryError> {
     let rows: Vec<_> = pattern.split('\n').collect();
-    let row = reflection_row(&rows);
+    let row = reflection_row(&rows, expected_distance);
     if let Some(row) = row {
-        return 100 * row;
+        return Ok(100 * row);
     }
 
     let cols = transpose(&rows);
     let cols_as_str_slices: Vec<&str> = cols.iter().map(AsRef::as_ref).collect();
-    let col = reflection_row(&cols_as_str_slices);
+    let col = reflection_row(&cols_as_str_slices, expected_distance);
     if let Some(col) = col {
-        return col;
+        return Ok(col);
     }
 
-    panic!("no reflection found for pattern: {}", pattern);
+    Err(SummaryError::ReflectionNotFound(pattern.to_owned()))
 }
 
 fn part1(input: &str) -> i32 {
-    input.split("\n\n").map(|pattern| summarize(pattern)).sum()
+    input
+        .split("\n\n")
+        .map(|pattern| summarize(pattern, 0).expect("pattern should have a reflection"))
+        .sum()
 }
 
-fn part2(input: &str) -> () {}
+fn part2(input: &str) -> i32 {
+    input
+        .split("\n\n")
+        .map(|pattern| summarize(pattern, 1).expect("pattern should have a relection"))
+        .sum()
+}
 
 fn main() {
     dbg!(part1(INPUT));
-    // dbg!(part2(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -77,9 +99,9 @@ mod tests {
         assert_eq!(part1(INPUT), 29213);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(EXAMPLE), ());
-    //     assert_eq!(part2(INPUT), ());
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE), 400);
+        assert_eq!(part2(INPUT), 37453);
+    }
 }
