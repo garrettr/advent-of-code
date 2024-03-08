0b71906bc6267b3c6a333b010fbe0ba69c0ea2af
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-07 22:51:55 -0800

    2021, Day 2, Part 2, Rust

--- rust/2021/day-02/src/main.rs
+++ rust/2021/day-02/src/main.rs
@@ -18,11 +18,33 @@ fn part1(input: &str) -> i32 {
     commands["forward"] * (commands["down"] - commands["up"])
 }
 
-fn part2(input: &str) -> () {}
+fn part2(input: &str) -> i32 {
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in input.lines() {
+        let (command, value) = line
+            .split_once(" ")
+            .expect("two strings separated by a space");
+        let value: i32 = value.parse().expect("value is an integer");
+        match command {
+            "down" => aim += value,
+            "up" => aim -= value,
+            "forward" => {
+                horizontal_position += value;
+                depth += aim * value;
+            }
+            _ => panic!("Invalid command: {}", command),
+        }
+    }
+
+    horizontal_position * depth
+}
 
 fn main() {
-    dbg!(part1(EXAMPLE));
-    // dbg!(part2(INPUT));
+    dbg!(part1(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -37,7 +59,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        // assert_eq!(part2(EXAMPLE), ());
-        // assert_eq!(part2(INPUT), ());
+        assert_eq!(part2(EXAMPLE), 900);
+        assert_eq!(part2(INPUT), 1544000595);
     }
 }
