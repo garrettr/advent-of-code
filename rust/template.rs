fa00d7e0968010a83243aa60c079791a7d6e3f5b
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-07 22:44:42 -0800

    Rust solution template defaults to returning i64

--- rust/template.rs
+++ rust/template.rs
@@ -1,9 +1,9 @@
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
-fn part1(input: &str) -> () {}
+fn part1(input: &str) -> i64 {}
 
-fn part2(input: &str) -> () {}
+fn part2(input: &str) -> i64 {}
 
 fn main() {
     dbg!(part1(EXAMPLE));
@@ -16,13 +16,13 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(EXAMPLE), ());
-        // assert_eq!(part1(INPUT), ());
+        assert_eq!(part1(EXAMPLE), 0);
+        // assert_eq!(part1(INPUT), 0);
     }
 
     #[test]
     fn test_part2() {
-        // assert_eq!(part2(EXAMPLE), ());
-        // assert_eq!(part2(INPUT), ());
+        // assert_eq!(part2(EXAMPLE), 0);
+        // assert_eq!(part2(INPUT), 0);
     }
 }
