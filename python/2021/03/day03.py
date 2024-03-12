00dd596db0453b51848ce078c51aec125d9f6461
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-12 10:30:28 -0700

    2021, Day 3, Part 2, Python

--- python/2021/03/day03.py
+++ python/2021/03/day03.py
@@ -21,8 +21,38 @@ def part1(input: str):
     return gamma_rate * epsilon_rate
 
 
+def most_common_value(values):
+    values_sorted_by_count = Counter(values).most_common()
+    if values_sorted_by_count[0][1] == values_sorted_by_count[1][1]:
+        return "1"
+    return values_sorted_by_count[0][0]
+
+
+def least_common_value(values):
+    values_sorted_by_count = Counter(values).most_common()
+    if values_sorted_by_count[0][1] == values_sorted_by_count[1][1]:
+        return "0"
+    return values_sorted_by_count[1][0]
+
+
 def part2(input: str):
-    pass
+    def filter_values(values: list[int], position: int, criterion):
+        if len(values) == 1:
+            return values[0]
+        values_by_position = list(zip(*values))
+        required_value = criterion(values_by_position[position])
+        filtered_values = [
+            value for value in values if value[position] == required_value
+        ]
+        # print(filtered_values)
+        return filter_values(filtered_values, position + 1, criterion)
+
+    values = parse(input)
+    o2_generator_rating = filter_values(values, 0, most_common_value)
+    co2_scrubber_rating = filter_values(values, 0, least_common_value)
+    o2_generator_rating = int("".join(o2_generator_rating), 2)
+    co2_scrubber_rating = int("".join(co2_scrubber_rating), 2)
+    return o2_generator_rating * co2_scrubber_rating
 
 
 class TestDay3(unittest.TestCase):
@@ -34,9 +64,9 @@ class TestDay3(unittest.TestCase):
         self.assertEqual(part1(self.example), 198)
         self.assertEqual(part1(self.input), 2972336)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 230)
+        self.assertEqual(part2(self.input), None)
 
 
 if __name__ == "__main__":
