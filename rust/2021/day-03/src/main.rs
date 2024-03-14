56b7452731598fed851374b92812115ae4dd1be8
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-13 17:55:25 -0700

    2021, Day 3, Part 2, Rust

--- rust/2021/day-03/src/main.rs
+++ rust/2021/day-03/src/main.rs
@@ -1,5 +1,3 @@
-use std::convert;
-
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
@@ -10,7 +8,7 @@ fn parse(input: &str) -> Vec<Vec<u32>> {
         .collect()
 }
 
-fn transpose(matrix: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+fn transpose(matrix: &Vec<Vec<u32>>) -> Vec<Vec<u32>> {
     let mut result = vec![];
     for i in 0..matrix[0].len() {
         let mut row = vec![];
@@ -27,7 +25,9 @@ fn most_common_bit(row: &[u32]) -> u32 {
     for bit in row {
         count[*bit as usize] += 1;
     }
-    if count[0] > count[1] {
+    if count[0] == count[1] {
+        1
+    } else if count[0] > count[1] {
         0
     } else {
         1
@@ -35,7 +35,13 @@ fn most_common_bit(row: &[u32]) -> u32 {
 }
 
 fn least_common_bit(row: &[u32]) -> u32 {
-    if most_common_bit(row) == 0 {
+    let mut count = vec![0; 2];
+    for bit in row {
+        count[*bit as usize] += 1;
+    }
+    if count[0] == count[1] {
+        0
+    } else if count[0] > count[1] {
         1
     } else {
         0
@@ -52,19 +58,39 @@ fn find_gamma_or_epsilon_rate(matrix: &Vec<Vec<u32>>, eval: fn(&[u32]) -> u32) -
 }
 
 fn part1(input: &str) -> u32 {
-    let matrix = transpose(parse(input));
+    let matrix = transpose(&parse(input));
     let gamma_rate = find_gamma_or_epsilon_rate(&matrix, most_common_bit);
     let epsilon_rate = find_gamma_or_epsilon_rate(&matrix, least_common_bit);
     gamma_rate * epsilon_rate
 }
 
+fn find_o2_generator_or_co2_scrubber_rating(
+    bits: &Vec<Vec<u32>>,
+    bit_criterion: fn(&[u32]) -> u32,
+) -> u32 {
+    let mut bits = bits.clone();
+    let mut bits_by_position = transpose(&bits);
+    for position in 0..bits_by_position.len() {
+        if bits.len() == 1 {
+            break;
+        }
+        let bit = bit_criterion(&bits_by_position[position]);
+        bits.retain(|row| row[position] == bit);
+        bits_by_position = transpose(&bits);
+    }
+    convert_bits_to_int(&bits[0])
+}
+
 fn part2(input: &str) -> u32 {
-    0
+    let bits = parse(input);
+    let o2_generator_rating = find_o2_generator_or_co2_scrubber_rating(&bits, most_common_bit);
+    let co2_scrubber_rating = find_o2_generator_or_co2_scrubber_rating(&bits, least_common_bit);
+    o2_generator_rating * co2_scrubber_rating
 }
 
 fn main() {
-    dbg!(part1(EXAMPLE));
-    // dbg!(part2(INPUT));
+    dbg!(part1(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -83,19 +109,25 @@ mod tests {
     #[test]
     fn test_transpose() {
         assert_eq!(
-            transpose(vec![vec![1, 2, 3], vec![4, 5, 6]]),
+            transpose(&vec![vec![1, 2, 3], vec![4, 5, 6]]),
             vec![vec![1, 4], vec![2, 5], vec![3, 6]]
         );
     }
 
     #[test]
     fn test_most_common_bit() {
+        assert_eq!(most_common_bit(&[0, 0, 1]), 0);
+        assert_eq!(most_common_bit(&[0, 1, 1]), 1);
+        assert_eq!(most_common_bit(&[0, 1, 0, 1]), 1);
         assert_eq!(most_common_bit(&[0, 1, 1, 0, 1, 0]), 1);
         assert_eq!(most_common_bit(&[0, 1, 0, 0, 1, 0]), 0);
     }
 
     #[test]
     fn test_least_common_bit() {
+        assert_eq!(least_common_bit(&[0, 0, 1]), 1);
+        assert_eq!(least_common_bit(&[0, 1, 1]), 0);
+        assert_eq!(least_common_bit(&[0, 1, 0, 1]), 0);
         assert_eq!(least_common_bit(&[0, 1, 1, 0, 1, 0]), 0);
         assert_eq!(least_common_bit(&[0, 1, 0, 0, 1, 0]), 1);
     }
@@ -110,9 +142,23 @@ mod tests {
 
     #[test]
     fn test_find_gamma_or_epsilon_rate() {
-        let matrix = transpose(parse(EXAMPLE));
+        let matrix = transpose(&parse(EXAMPLE));
         assert_eq!(find_gamma_or_epsilon_rate(&matrix, most_common_bit), 22);
         assert_eq!(find_gamma_or_epsilon_rate(&matrix, least_common_bit), 9);
+    }
+
+    #[test]
+    fn test_find_o2_generator_or_co2_scrubber_rating() {
+        let bits = parse(EXAMPLE);
+        dbg!(&bits);
+        assert_eq!(
+            find_o2_generator_or_co2_scrubber_rating(&bits, most_common_bit),
+            23
+        );
+        assert_eq!(
+            find_o2_generator_or_co2_scrubber_rating(&bits, least_common_bit),
+            10
+        );
     }
 
     #[test]
@@ -123,7 +169,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        // assert_eq!(part2(EXAMPLE), 0);
-        // assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(EXAMPLE), 230);
+        assert_eq!(part2(INPUT), 3368358);
     }
 }
