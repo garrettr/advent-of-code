c79b75d4efa80a808b1a4b64a5fb7f8c5da983df
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-17 16:04:33 -0700

    2021, Day 4, Part 2, Python

--- python/2021/04/day04.py
+++ python/2021/04/day04.py
@@ -53,7 +53,14 @@ def part1(input: str):
 
 
 def part2(input: str):
-    pass
+    numbers, boards = parse(input)
+    for i in range(5, len(numbers)):
+        drawn = numbers[:i]
+        last_board = boards[-1]
+        boards = [board for board in boards if not won(board, drawn)]
+        if len(boards) == 0:
+            return score(last_board, drawn)
+    raise Exception("No solution found!")
 
 
 class TestDay4(unittest.TestCase):
@@ -65,9 +72,9 @@ class TestDay4(unittest.TestCase):
         self.assertEqual(part1(self.example), 4512)
         self.assertEqual(part1(self.input), 58412)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 1924)
+        self.assertEqual(part2(self.input), 10030)
 
 
 if __name__ == "__main__":
