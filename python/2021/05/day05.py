72dcfe2cadbea21aec2289f99f3caa13f18c6323
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-18 12:49:01 -0700

    2021, Day 5, Part 2, Python

--- python/2021/05/day05.py
+++ python/2021/05/day05.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from collections import Counter
-from itertools import chain
+from itertools import chain, repeat
 import unittest
 
 from advent import get_puzzle_input
@@ -28,12 +28,15 @@ def is_vertical_or_horizontal_line(line: Line) -> bool:
 
 def points_on(line) -> list[GridPoint]:
     (x1, y1), (x2, y2) = line
-    if x1 == x2:
-        step = 1 if y1 < y2 else -1
-        return [(x1, y) for y in range(y1, y2 + step, step)]
-    elif y1 == y2:
-        step = 1 if x1 < x2 else -1
-        return [(x, y1) for x in range(x1, x2 + step, step)]
+    x_step = 1 if x1 < x2 else -1
+    y_step = 1 if y1 < y2 else -1
+    return [
+        (x, y)
+        for x, y in zip(
+            range(x1, x2 + x_step, x_step) if x1 != x2 else repeat(x1),
+            range(y1, y2 + y_step, y_step) if y1 != y2 else repeat(y2),
+        )
+    ]
 
 
 def part1(input: str):
@@ -48,7 +51,13 @@ def part1(input: str):
 
 
 def part2(input: str):
-    pass
+    lines = parse(input)
+    points = [points_on(line) for line in lines]
+    counter = Counter(chain(*points))
+    points_with_two_or_more_overlapping_lines = [
+        point for (point, count) in counter.most_common() if count > 1
+    ]
+    return len(points_with_two_or_more_overlapping_lines)
 
 
 class TestDay5(unittest.TestCase):
@@ -65,14 +74,16 @@ class TestDay5(unittest.TestCase):
     def test_points_on(self):
         self.assertEqual(points_on(((0, 0), (0, 2))), [(0, 0), (0, 1), (0, 2)])
         self.assertEqual(points_on(((0, 0), (3, 0))), [(0, 0), (1, 0), (2, 0), (3, 0)])
+        self.assertEqual(points_on(((1, 1), (3, 3))), [(1, 1), (2, 2), (3, 3)])
+        self.assertEqual(points_on(((9, 7), (7, 9))), [(9, 7), (8, 8), (7, 9)])
 
     def test_part1(self):
         self.assertEqual(part1(self.example), 5)
         self.assertEqual(part1(self.input), 7414)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 12)
+        self.assertEqual(part2(self.input), 19676)
 
 
 if __name__ == "__main__":
