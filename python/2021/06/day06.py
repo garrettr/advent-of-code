b84fe27a95cd6182902abb62b24dbbcb5df7fc60
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-26 11:43:31 -0700

    2021, Day 6, Part 2, Python

--- python/2021/06/day06.py
+++ python/2021/06/day06.py
@@ -11,6 +11,16 @@ def parse(input: str) -> list[int]:
     return [int(x) for x in input.strip().split(",")]
 
 
+def parse2(input: str) -> list[int]:
+    # Instead of tracking each individual fish, just keep track of the sum of
+    # fish per day in the 7 day cycle. Add 2 days to the cycle for new fish,
+    # which spawn with their internal timer set to 8 days.
+    cycle = [0] * 9
+    for fish in input.strip().split(","):
+        cycle[int(fish)] += 1
+    return cycle
+
+
 def simulate(fishes: list[int], days: int):
     for day in range(days):
         new_fishes = []
@@ -24,6 +34,22 @@ def simulate(fishes: list[int], days: int):
     return fishes
 
 
+def simulate2(cycle: list[int], days: int):
+    for _ in range(days):
+        # Aging
+        prev_day_count = cycle[-1]
+        for day in reversed(range(1, len(cycle))):
+            curr_day_count = prev_day_count
+            prev_day_count = cycle[day - 1]
+            cycle[day] -= curr_day_count
+            cycle[day - 1] += curr_day_count
+        # Spawning
+        cycle[0] -= prev_day_count
+        cycle[6] += prev_day_count
+        cycle[8] += prev_day_count
+    return cycle
+
+
 def part1(input: str) -> int:
     fishes = parse(input)
     simulate(fishes, 80)
@@ -31,7 +57,9 @@ def part1(input: str) -> int:
 
 
 def part2(input: str):
-    pass
+    cycle = parse2(input)
+    simulate2(cycle, 256)
+    return sum(cycle)
 
 
 class TestDay6(unittest.TestCase):
@@ -42,13 +70,20 @@ class TestDay6(unittest.TestCase):
     def test_simulate(self):
         self.assertEqual(len(simulate(parse(self.example), 18)), 26)
 
+    def test_parse2(self):
+        self.assertEqual(parse2(self.example), [0, 1, 1, 2, 1, 0, 0, 0, 0])
+
     def test_part1(self):
         self.assertEqual(part1(self.example), 5934)
         self.assertEqual(part1(self.input), 391671)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_simulate2(self):
+        self.assertEqual(sum(simulate2(parse2(self.example), 80)), 5934)
+        self.assertEqual(sum(simulate2(parse2(self.input), 80)), 391671)
+
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 26984457539)
+        self.assertEqual(part2(self.input), 1754000560399)
 
 
 if __name__ == "__main__":
