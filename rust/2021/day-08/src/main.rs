6a1ba607d218ceb3c1e0e538d24fafd6e2039da4
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-30 13:30:37 -0700

    2021, Day 8, Part 2, Rust

--- rust/2021/day-08/src/main.rs
+++ rust/2021/day-08/src/main.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+use frozenset::{Freeze, FrozenSet};
 
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
@@ -7,6 +9,12 @@ const INPUT: &str = include_str!("input.txt");
 struct Entry<'a> {
     signal_patterns: [&'a str; 10],
     output_value: [&'a str; 4],
+}
+
+fn digits_to_int(digits: &[u8]) -> u64 {
+    digits.iter().rev().enumerate().fold(0, |acc, (i, &d)| {
+        acc + (d as u64) * 10u64.pow(i.try_into().unwrap())
+    })
 }
 
 impl<'a> Entry<'a> {
@@ -26,6 +34,76 @@ impl<'a> Entry<'a> {
             signal_patterns,
             output_value,
         }
+    }
+
+    fn decode_digits(&self) -> HashMap<FrozenSet<char>, u8> {
+        let mut patterns_by_len: HashMap<usize, Vec<HashSet<char>>> = HashMap::with_capacity(6);
+        for pattern in self.signal_patterns {
+            patterns_by_len
+                .entry(pattern.len())
+                .or_default()
+                .push(pattern.chars().collect());
+        }
+
+        let mut digits: HashMap<u8, HashSet<char>> = HashMap::with_capacity(10);
+        digits.insert(1, patterns_by_len.get_mut(&2).unwrap().remove(0));
+        digits.insert(4, patterns_by_len.get_mut(&4).unwrap().remove(0));
+        digits.insert(7, patterns_by_len.get_mut(&3).unwrap().remove(0));
+        digits.insert(8, patterns_by_len.get_mut(&7).unwrap().remove(0));
+
+        // There are two groups of remaining unidentified digits:
+        // Length 5: 2, 3, 5
+        // Length 6: 0, 6, 9
+
+        // Of the 3 length 6 digits, 0 and 9 are a superset of the known digit 7,
+        // while 6 is not.
+        let v = patterns_by_len.get_mut(&6).unwrap();
+        let i = v
+            .iter()
+            .position(|digit| !digit.is_superset(&digits[&7]))
+            .unwrap();
+        digits.insert(6, v.swap_remove(i));
+        // Of the remaining length 6 digits, 0 and 9, 9 is a superset of 4 while
+        // 0 is not.
+        let i = v
+            .iter()
+            .position(|digit| digit.is_superset(&digits[&4]))
+            .unwrap();
+        digits.insert(9, v.swap_remove(i));
+        assert!(v.len() == 1);
+        digits.insert(0, v.swap_remove(0));
+
+        // Similarly, of the 3 length 5 digits, only 3 is a superset of 7.
+        let v = patterns_by_len.get_mut(&5).unwrap();
+        let i = v
+            .iter()
+            .position(|digit| digit.is_superset(&digits[&7]))
+            .unwrap();
+        digits.insert(3, v.swap_remove(i));
+        // Of the remaining length 5 digits, 2 and 5, only digit 5 contains
+        // segments b and d. We can isolate segments b and d by taking the
+        // difference of digit 4 - digit 1.
+        let bd = &digits[&4] - &digits[&1];
+        let i = v.iter().position(|digit| digit.is_superset(&bd)).unwrap();
+        digits.insert(5, v.swap_remove(i));
+        assert!(v.len() == 1);
+        digits.insert(2, v.swap_remove(0));
+
+        digits
+            .drain()
+            .map(|(digit, chars)| (chars.freeze(), digit))
+            .collect()
+    }
+
+    fn decode(&self) -> u64 {
+        let digits = self.decode_digits();
+        let output_value: Vec<u8> = self
+            .output_value
+            .iter()
+            .map(|digit| digit.chars().collect::<HashSet<_>>().freeze())
+            .map(|digit| *digits.get(&digit).unwrap())
+            .collect();
+        digits_to_int(&output_value)
     }
 }
 
@@ -47,12 +125,13 @@ fn part1(input: &str) -> u64 {
 }
 
 fn part2(input: &str) -> u64 {
-    0
+    let entries = parse(input);
+    entries.iter().map(|entry| entry.decode()).sum()
 }
 
 fn main() {
-    dbg!(part1(EXAMPLE));
-    // dbg!(part2(EXAMPLE));
+    dbg!(part1(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -67,7 +146,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        // assert_eq!(part2(EXAMPLE), 0);
-        // assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(EXAMPLE), 61229);
+        assert_eq!(part2(INPUT), 1023686);
     }
 }
