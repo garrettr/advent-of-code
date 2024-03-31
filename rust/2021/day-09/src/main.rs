8b66cafa6604cdeed7e9cb83cda22162c91d0e55
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-31 14:24:33 -0700

    2021, Day 9, Part 2, Rust

--- rust/2021/day-09/src/main.rs
+++ rust/2021/day-09/src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const EXAMPLE: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct GridPoint(i8, i8);
 
 #[derive(Debug)]
@@ -34,7 +34,7 @@ impl Grid {
         adjacent
     }
 
-    fn find_low_points(&self) -> Vec<&GridPoint> {
+    fn find_low_points(&self) -> Vec<GridPoint> {
         let mut low_points = Vec::new();
         for (point, height) in self.0.iter() {
             let heights = self
@@ -43,10 +43,32 @@ impl Grid {
                 .map(|point| self.0[point])
                 .collect::<Vec<_>>();
             if height < heights.iter().min().unwrap() {
-                low_points.push(point);
+                low_points.push(point.clone());
             }
         }
         low_points
+    }
+
+    fn find_basins(&self) -> Vec<HashSet<GridPoint>> {
+        let mut basins = Vec::new();
+        for low_point in self.find_low_points() {
+            let mut basin: HashSet<GridPoint> = HashSet::new();
+            let mut candidates = vec![low_point];
+            while !candidates.is_empty() {
+                let point = candidates.pop().unwrap();
+                if self.0[&point] < 9 {
+                    basin.insert(point);
+                    let adjacent_points = self
+                        .find_adjacent(&point)
+                        .into_iter()
+                        .collect::<HashSet<_>>();
+                    let unvisted = &adjacent_points - &basin;
+                    candidates.extend(unvisted.into_iter());
+                }
+            }
+            basins.push(basin);
+        }
+        basins
     }
 }
 
@@ -60,12 +82,20 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    0
+    let grid = Grid::from_str(input);
+    let mut basins = grid.find_basins();
+    basins.sort_by_key(|basin| basin.len());
+    basins
+        .iter()
+        .rev()
+        .take(3)
+        .map(|basin| basin.len() as u32)
+        .product()
 }
 
 fn main() {
     dbg!(part1(INPUT));
-    // dbg!(part2(EXAMPLE));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -80,7 +110,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        // assert_eq!(part2(EXAMPLE), 0);
-        // assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(EXAMPLE), 1134);
+        assert_eq!(part2(INPUT), 1391940);
     }
 }
