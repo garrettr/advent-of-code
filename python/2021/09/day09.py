ae393758bff4fdbf9f02b6051b3b90f60929ded4
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-03-31 12:28:02 -0700

    2021, Day 9, Part 2, Python

--- python/2021/09/day09.py
+++ python/2021/09/day09.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from functools import reduce
+from operator import mul
 import unittest
 
 from advent import get_puzzle_input
@@ -38,19 +40,41 @@ def find_adjacent_points(grid: Grid, point: GridPoint) -> list[GridPoint]:
     return [candidate for candidate in find_candidates(point) if candidate in grid]
 
 
-def part1(input: str):
-    grid = parse_grid(input.splitlines())
-    low_points_heights = []
+def find_low_points(grid: Grid) -> list[GridPoint]:
+    low_points = []
     for point, height in grid.items():
         adjacent_points = find_adjacent_points(grid, point)
         heights = [grid[point] for point in adjacent_points]
         if height < min(heights):
-            low_points_heights.append(height)
-    return sum(height + 1 for height in low_points_heights)
+            low_points.append(point)
+    return low_points
+
+
+def part1(input: str):
+    grid = parse_grid(input.splitlines())
+    low_points = find_low_points(grid)
+    return sum(grid[point] + 1 for point in low_points)
+
+
+def find_basins(grid: Grid) -> list[set[GridPoint]]:
+    basins = []
+    for low_point in find_low_points(grid):
+        basin = set()
+        candidates = set([low_point])
+        while candidates:
+            point = candidates.pop()
+            if grid[point] < 9:
+                basin.add(point)
+                candidates.update(set(find_adjacent_points(grid, point)) - basin)
+        basins.append(basin)
+    return basins
 
 
 def part2(input: str):
-    pass
+    grid = parse_grid(input.splitlines())
+    basins = find_basins(grid)
+    basins.sort(key=len, reverse=True)
+    return reduce(mul, (len(basin) for basin in basins[:3]))
 
 
 class TestDay9(unittest.TestCase):
@@ -62,9 +86,9 @@ class TestDay9(unittest.TestCase):
         self.assertEqual(part1(self.example), 15)
         self.assertEqual(part1(self.input), 458)
 
-    # def test_part2(self):
-    #     self.assertEqual(part2(self.example), None)
-    #     self.assertEqual(part2(self.input), None)
+    def test_part2(self):
+        self.assertEqual(part2(self.example), 1134)
+        self.assertEqual(part2(self.input), 1391940)
 
 
 if __name__ == "__main__":
