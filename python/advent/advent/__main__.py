6765d3195464202f7299594dea4fedfec63c95eb
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-04-29 19:07:04 -0700

    Refactor `new` subcommand into a new Puzzle class

--- python/advent/advent/__main__.py
+++ python/advent/advent/__main__.py
@@ -1,101 +1,17 @@
 #!/usr/bin/env python3
 import argparse
-import shutil
-import subprocess
 import sys
 from datetime import date
 
-from .puzzle import get_challenges_path, get_challenge_path
-from .website import download_puzzle_input
+from .puzzle import Puzzle, Language
 
 
-def get_current_puzzle() -> tuple[int, int]:
-    """Get the year and day of the most recently published Advent of Code puzzle."""
+def get_latest_puzzle() -> tuple[int, int]:
+    """Get the year and day of the latest Advent of Code puzzle."""
     today = date.today()
     year = today.year - (0 if today.month == 12 else 1)
     day = min(today.day, 25) if today.month == 12 else 25
     return year, day
-
-
-def setup_python(year: int, day: int, challenge_path: str):
-    # Create challenge directory.
-    challenge_path.mkdir(parents=True)
-
-    # Copy template file, substituting the current year and day.
-    with open(get_challenges_path("python") / "template.py") as f:
-        template = f.read()
-        template = template.replace("YEAR = 2023", f"YEAR = {year}")
-        template = template.replace("DAY = 1", f"DAY = {day}")
-        template = template.replace("TestDay", f"TestDay{day}")
-        solution_src = challenge_path / f"day{day:02d}.py"
-        with open(solution_src, "w") as f:
-            f.write(template)
-        solution_src.chmod(0o755)
-
-
-def setup_rust(year: int, day: int, challenge_path: str):
-    # Create parent directory (`rust/<year>`).
-    try:
-        challenge_path.parent.mkdir(parents=True)
-    except FileExistsError:
-        pass
-
-    # Create challenge directory and boilerplate binary package with Cargo.
-    try:
-        subprocess.run(
-            ["cargo", "new", challenge_path.name],
-            capture_output=True,
-            check=True,
-            cwd=challenge_path.parent,
-        )
-    except subprocess.CalledProcessError as e:
-        print(e.stderr.decode("utf-8"))
-        raise
-
-    # Copy template file.
-    shutil.copyfile(
-        get_challenges_path("rust") / "template.rs",
-        challenge_path / "src" / "main.rs"
-    )
-
-
-def do_language_specific_setup(language: str, year: int, day: int, challenge_path: str):
-    if language == "python":
-        setup = setup_python
-    elif language == "rust":
-        setup = setup_rust
-    else:
-        raise RuntimeError(f"Unsupported language: {language}")
-    setup(year, day, challenge_path)
-
-
-def new(language: str, year: int, day: int):
-    """Prepare for a new day's challenge."""
-    challenges_path = get_challenges_path(language)
-    if not challenges_path.exists():
-        res = input(f"Challenges directory {challenges_path} does not exist, create it? (y/n) ")
-        if res.lower() == "y":
-            challenges_path.mkdir(parents=True)
-        else:
-            return
-
-    challenge_path = get_challenge_path(language, year, day)
-    if challenge_path.exists():
-        res = input(f"Challenge directory {challenge_path} already exists, replace it? (y/n) ")
-        if res.lower() == "y":
-            shutil.rmtree(challenge_path)
-        else:
-            return
-
-    do_language_specific_setup(language, year, day, challenge_path)
-
-    if language == "rust":
-        input_dir = challenge_path / "src"
-    else:
-        input_dir = challenge_path
-    puzzle_input = download_puzzle_input(year, day)
-    with open(input_dir / "input.txt", "w") as f:
-        f.write(puzzle_input)
 
 
 def main():
@@ -105,15 +21,15 @@ def main():
         title="subcommands",
     )
 
-    new_cmd = subparsers.add_parser("new", help="Prepare for a new day's challenge")
+    new_cmd = subparsers.add_parser("new", help="Prepare for a new day's puzzle")
     new_cmd.add_argument(
         "language",
-        choices=["python", "rust"],
+        choices=[str(language) for language in Language],
         nargs="?",
         default="python",
-        help="Language to use",
+        help="Programming language to use for solution implementation",
     )
-    (year, day) = get_current_puzzle()
+    year, day = get_latest_puzzle()
     new_cmd.add_argument(
         "year", type=int, nargs="?", default=year, help="Year the puzzle was published"
     )
@@ -126,7 +42,8 @@ def main():
     try:
         match args.subcommand:
             case "new":
-                new(args.language, args.year, args.day)
+                puzzle = Puzzle(args.language, args.year, args.day)
+                puzzle.new()
     except Exception as e:
         print(e)
         return 1
--- python/advent/advent/puzzle.py
+++ python/advent/advent/puzzle.py
@@ -1,5 +1,10 @@
+from dataclasses import dataclass
+from enum import StrEnum, auto
 import pathlib
+import shutil
 import subprocess
+
+from .website import download_puzzle_input
 
 
 def repo_root():
@@ -17,21 +22,118 @@ def repo_root():
     return pathlib.Path(result.stdout.decode("utf-8").strip())
 
 
-def get_challenges_path(language: str) -> pathlib.Path:
+def get_puzzles_path_for_language(language: str) -> pathlib.Path:
     """Return the path to the subdirectory of solutions to challenges written in `language`."""
     return repo_root() / language
 
 
-def get_challenge_path(language: str, year: int, day: int):
-    """Return the path to the subdirectory containing the solution for a specific challenge."""
-    if language == "rust":
-        day_dir = f"day-{day:02d}"
-    else:
-        day_dir = f"{day:02d}"
-    return get_challenges_path(language) / str(year) / day_dir
+class Language(StrEnum):
+    PYTHON = auto()
+    RUST = auto()
 
 
-def get_puzzle_input(year: int, day: int, filename="input.txt") -> str:
-    """Get the puzzle input for the given year and day."""
-    with open(get_challenge_path("python", year, day) / filename) as f:
-        return f.read()
+@dataclass
+class Puzzle:
+    "Represents the solution of an Advent of Code puzzle in a programming language."
+    language: Language
+    year: int
+    day: int
+
+    @property
+    def path(self):
+        "Path to the subdirectory containing the solution for a specific puzzle in a specific language."
+        if self.language == Language.RUST:
+            day = f"day-{self.day:02d}"
+        else:
+            day = f"{self.day:02d}"
+        return get_puzzles_path_for_language(self.language) / str(self.year) / day
+
+    @property
+    def inputs_dir(self):
+        """Path to the subdirectory containing puzzle inputs as text files."""
+        if self.language == Language.RUST:
+            inputs_dir = self.path / "src"
+        else:
+            inputs_dir = self.path
+        return inputs_dir
+
+    INPUT_FILENAME = "input.txt"
+
+    @property
+    def input(self, filename=INPUT_FILENAME):
+        "Get the contents of a file in the puzzle's inputs directory."
+        with open(self.inputs_dir / filename) as f:
+            return f.read()
+
+    def _setup_python(self):
+        # Create puzzle directory.
+        self.path.mkdir(parents=True)
+
+        # Copy template file, substituting the current year and day.
+        with open(get_puzzles_path_for_language("python") / "template.py") as f:
+            template = f.read()
+            template = template.replace("YEAR = 2023", f"YEAR = {self.year}")
+            template = template.replace("DAY = 1", f"DAY = {self.day}")
+            template = template.replace("TestDay", f"TestDay{self.day}")
+            solution_src = self.path / f"day{self.day:02d}.py"
+            with open(solution_src, "w") as f:
+                f.write(template)
+            solution_src.chmod(0o755)
+
+    def _setup_rust(self):
+        # Create parent directory (`rust/<year>`).
+        try:
+            self.path.parent.mkdir(parents=True)
+        except FileExistsError:
+            pass
+
+        # Create challenge directory and boilerplate binary package with Cargo.
+        try:
+            subprocess.run(
+                ["cargo", "new", self.path.name],
+                capture_output=True,
+                check=True,
+                cwd=self.path.parent,
+            )
+        except subprocess.CalledProcessError as e:
+            print(e.stderr.decode("utf-8"))
+            raise
+
+        # Copy template file.
+        shutil.copyfile(
+            get_puzzles_path_for_language("rust") / "template.rs",
+            self.path / "src" / "main.rs"
+        )
+
+    def _download_puzzle_input(self):
+        puzzle_input = download_puzzle_input(self.year, self.day)
+        with open(self.inputs_dir / self.INPUT_FILENAME, "w") as f:
+            f.write(puzzle_input)
+
+    def new(self):
+        puzzles_path = get_puzzles_path_for_language(self.language)
+        if not puzzles_path.exists():
+            response = input(f"Puzzles path {puzzles_path} does not exist, create it? (y/n) ")
+            if response.lower() == "y":
+                puzzles_path.mkdir(parents=True)
+            else:
+                return
+
+        if self.path.exists():
+            response = input(f"Puzzle directory {self.path} already exists, replace it? (y/n) ")
+            if response.lower() == "y":
+                shutil.rmtree(self.path)
+
+        if self.language == Language.PYTHON:
+            self._setup_python()
+        elif self.language == Language.RUST:
+            self._setup_rust()
+        else:
+            raise RuntimeError(f"Unsupported language: {self.language}")
+
+        self._download_puzzle_input()
+        
+
+def get_puzzle_input(year: int, day: int, filename=Puzzle.INPUT_FILENAME) -> str:
+    """Helper function for Python solutions that gets the puzzle input for the given year and day."""
+    return Puzzle(language.PYTHON, year, day).input(filename)
