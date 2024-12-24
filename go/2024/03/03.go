ebc566e1ad9b3ebcc69927a218f8ad49f1716436
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-12-24 11:50:49 -0800

    Refactor condition processing into separate function

--- go/2024/03/03.go
+++ go/2024/03/03.go
@@ -31,31 +31,34 @@ func solvePart1(input string) (result int) {
 	return
 }
 
+func processConditions(input string, conds [][]int, condIndex *int, mulPos int, enabled bool) bool {
+	for ; *condIndex < len(conds) && conds[*condIndex][0] < mulPos; *condIndex++ {
+		cond := input[conds[*condIndex][0]:conds[*condIndex][1]]
+		switch cond {
+		case "do()":
+			enabled = true
+		case "don't()":
+			enabled = false
+		default:
+			log.Fatalf("expected do() or don't(), got %v", cond)
+		}
+	}
+	return enabled
+}
+
 func solvePart2(input string) (result int) {
 	muls := multiplyPattern.FindAllStringSubmatchIndex(input, -1)
 	conds := conditionPattern.FindAllStringSubmatchIndex(input, -1)
-
-	isEnabled := true
+	enabled := true
 	condIndex := 0
 	for _, mul := range muls {
-		for ; condIndex < len(conds) && conds[condIndex][0] < mul[0]; condIndex++ {
-			switch cond := input[conds[condIndex][0]:conds[condIndex][1]]; cond {
-			case "do()":
-				isEnabled = true
-			case "don't()":
-				isEnabled = false
-			default:
-				log.Fatalf("expected do() or don't(), got %v", cond)
-			}
-		}
-
-		if isEnabled {
+		enabled = processConditions(input, conds, &condIndex, mul[0], enabled)
+		if enabled {
 			x, _ := strconv.Atoi(input[mul[2]:mul[3]])
 			y, _ := strconv.Atoi(input[mul[4]:mul[5]])
 			result += x * y
 		}
 	}
-
 	return
 }
 
