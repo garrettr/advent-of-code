cca1d0db4ca9ac9a146c7b65698e8368f371d2e8
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-12-24 14:16:22 -0800

    Move table-driven test functions to advent module

--- go/2024/03/03_test.go
+++ go/2024/03/03_test.go
@@ -9,28 +9,13 @@ import (
 const example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 const example2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
-type TestCase struct {
-	name   string
-	solver func(string) int
-	input  string
-	want   int
-}
-
 func TestSolve(t *testing.T) {
 	input := advent.GetInput("input.txt")
-	testCases := []TestCase{
-		{"Part1_example", solvePart1, example, 161},
-		{"Part1_input", solvePart1, input, 184576302},
-		{"Part2_example", solvePart2, example2, 48},
-		{"Part2_input", solvePart2, input, 118173507},
+	testCases := []advent.TestCase{
+		{Name: "Part1_example", Solver: solvePart1, Input: example, Want: 161},
+		{Name: "Part1_input", Solver: solvePart1, Input: input, Want: 184576302},
+		{Name: "Part2_example", Solver: solvePart2, Input: example2, Want: 48},
+		{Name: "Part2_input", Solver: solvePart2, Input: input, Want: 118173507},
 	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			solution := tc.solver(tc.input)
-			if solution != tc.want {
-				t.Fatalf("got solution %v, want %v", solution, tc.want)
-			}
-		})
-	}
+	advent.RunSolveTests(t, testCases)
 }
--- go/advent/advent.go
+++ go/advent/advent.go
@@ -3,6 +3,7 @@ package advent
 import (
 	"log"
 	"os"
+	"testing"
 )
 
 func GetInput(fname string) string {
@@ -27,4 +28,22 @@ func Zip[T any](a, b []T) [][2]T {
 		result[i] = [2]T{a[i], b[i]}
 	}
 	return result
+}
+
+type TestCase struct {
+	Name   string
+	Solver func(string) int
+	Input  string
+	Want   int
+}
+
+func RunSolveTests(t *testing.T, testCases []TestCase) {
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			solution := tc.Solver(tc.Input)
+			if solution != tc.Want {
+				t.Fatalf("got solution %v, want %v", solution, tc.Want)
+			}
+		})
+	}
 }
