f958ed239576985d2913f69ed27c073632fe10e0
Garrett Robinson <garrett.f.robinson@gmail.com>
2024-12-23 21:05:57 -0800

    2024, Day 3, Part 2, Go

--- go/2024/03/03.go
+++ go/2024/03/03.go
@@ -27,9 +27,43 @@ func solvePart1(input string) (result int) {
 	return
 }
 
+func solvePart2(input string) (result int) {
+	mulRe := regexp.MustCompile(`mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
+	condRe := regexp.MustCompile(`do(?:n't)?\(\)`)
+
+	muls := mulRe.FindAllStringSubmatchIndex(input, -1)
+	conds := condRe.FindAllStringSubmatchIndex(input, -1)
+
+	isEnabled := true
+	condIndex := 0
+	for _, mul := range muls {
+		for ; condIndex < len(conds) && conds[condIndex][0] < mul[0]; condIndex++ {
+			switch cond := input[conds[condIndex][0]:conds[condIndex][1]]; cond {
+			case "do()":
+				isEnabled = true
+			case "don't()":
+				isEnabled = false
+			default:
+				log.Fatalf("expected do() or don't(), got %v", cond)
+			}
+		}
+
+		if isEnabled {
+			x, _ := strconv.Atoi(input[mul[2]:mul[3]])
+			y, _ := strconv.Atoi(input[mul[4]:mul[5]])
+			result += x * y
+		}
+	}
+
+	return
+}
+
 func main() {
 	input := getInput("input.txt")
 
 	solution1 := solvePart1(input)
 	fmt.Println(solution1)
+
+	solution2 := solvePart2(input)
+	fmt.Println(solution2)
 }
--- go/2024/03/03_test.go
+++ go/2024/03/03_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 const example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+const example2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 type TestCase struct {
 	name   string
@@ -18,8 +19,8 @@ func TestSolve(t *testing.T) {
 	testCases := []TestCase{
 		{"Part1_example", solvePart1, example, 161},
 		{"Part1_input", solvePart1, input, 184576302},
-		// {"Part2_example", solvePart2, example, 4},
-		// {"Part2_input", solvePart2, input, 689},
+		{"Part2_example", solvePart2, example2, 48},
+		{"Part2_input", solvePart2, input, 118173507},
 	}
 
 	for _, tc := range testCases {
